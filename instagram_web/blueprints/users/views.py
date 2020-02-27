bdae1e93b7067f09d5c4901a0d63f6042e997468
Kumar Subramaniam <55949190+sideprojex@users.noreply.github.com>
2020-02-27 16:00:39 +0800

    signup adding

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,36 +1,51 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from models.user import User
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 
 
-users_blueprint = Blueprint('users',
-                            __name__,
-                            template_folder='templates')
+users_blueprint = Blueprint("users", __name__, template_folder="templates")
 
 
-@users_blueprint.route('/new', methods=['GET'])
+@users_blueprint.route("/new", methods=["GET"])
 def new():
-    return render_template('users/new.html')
+    return render_template("users/new.html")
 
 
-@users_blueprint.route('/', methods=['POST'])
+@users_blueprint.route("/new", methods=["POST"])
 def create():
-    pass
+
+    user_name = request.form.get("user_name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # hashed_password = generate_password_hash(password)  # store this in the database
+
+    user = User(username=user_name, email=email, password=password)
+
+    if user.save():
+        flash("Succesfully Signed up!,")
+        return redirect(url_for("users.new"))
+    else:
+        flash("Error logging in,")
+        return redirect(url_for("home"))
 
 
-@users_blueprint.route('/<username>', methods=["GET"])
+@users_blueprint.route("/<username>", methods=["GET"])
 def show(username):
     pass
 
 
-@users_blueprint.route('/', methods=["GET"])
+@users_blueprint.route("/", methods=["GET"])
 def index():
     return "USERS"
 
 
-@users_blueprint.route('/<id>/edit', methods=['GET'])
+@users_blueprint.route("/<id>/edit", methods=["GET"])
 def edit(id):
     pass
 
 
-@users_blueprint.route('/<id>', methods=['POST'])
+@users_blueprint.route("/<id>", methods=["POST"])
 def update(id):
     pass
--- models/user.py
+++ models/user.py
@@ -1,6 +1,38 @@
 from models.base_model import BaseModel
 import peewee as pw
+import re
+from werkzeug.security import generate_password_hash
 
 
 class User(BaseModel):
-    name = pw.CharField(unique=False)
+    username = pw.CharField(unique=True)
+    email = pw.CharField(null=False)
+    password = pw.CharField(unique=True)
+
+    def validate(self):
+        # pattern = "[A-Za-z0-9@#$%^&+=]"
+        regexcheck = re.match(
+            r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-]).{6,}$", self.password
+        )
+        if len(self.password) <= 6:
+            self.errors.append("Password must be more than 6 characters")
+        elif not (regexcheck):
+            self.errors.append(
+                "Password must have atleast the following: one uppercase letter, one lowercase letter, and one special character"
+            )
+        else:
+            self.password = generate_password_hash(self.password)
+
+        # implement the validation from yesterday
+        duplicate_email = User.get_or_none(User.email == self.email)
+        # pass_check = User.get_or_none(User.password == self.password)
+        if duplicate_email:
+            self.errors.append("Email already taken")
+        else:
+            print("Validation is now being implemented")
+        return True
+
+        duplicate_username = User.get_or_none(User.username == self.username)
+        if duplicate_username:
+            self.errors.append("username not unique")
+
